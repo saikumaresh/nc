f694baf98c77cce943f853e7a4d1ed870ae5c5e3
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2021-03-24 16:26:46 +0530

    Update running.py

--- running.py
+++ running.py
@@ -1,6 +1,6 @@
 from ncclient import manager
 
-with manager.connect(host='10.10.20.48', port=830, username=cisco1, hostkey_verify=False) as m:
+with manager.connect(host='10.10.20.48', port=830, username='cisco1', password='cisco1' , hostkey_verify=False) as m:
     c = m.get_config(source='running').data_xml
-    with open("%s.xml" % host, 'w') as f:
+    with open('running_config.txt', 'w') as f:
         f.write(c)
