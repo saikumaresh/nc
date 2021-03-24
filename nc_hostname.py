fad40b96bae4b60e2ed9cdd5c6e0813b8d7a1730
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2021-03-24 16:00:24 +0530

    Update nc_hostname.py

--- nc_hostname.py
+++ nc_hostname.py
@@ -2,10 +2,6 @@ from ncclient import manager
 import xmltodict
 import xml.dom.minidom
 
-host = 'ios-xe-mgmt.cisco.com'
-user = 'developer'
-password = 'C1sco12345'
-port=10000
 payload = """
   <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
     <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
@@ -21,25 +17,7 @@ data = '''
     </native>
   </config>
 '''
-get_name=''''
-    <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-        <username>
-          <name/>
-        </username>
-      </native>
-    </filter>
-'''
 
-edit_name='''
-    <config>
-      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-        <username>
-          <name>___sk___</name>
-        </username>
-      </native>
-    </config>
-'''
   # Connect to device
 def csr_connect(host, port, user, password):
   return manager.connect(host=host,port=port,username=user,password=password,device_params={'name': "csr"},timeout=30)
@@ -56,11 +34,11 @@ def get_name(m):
 
 
 def edit_value(m):
-    #xmlDom = xml.dom.minidom.parseString( str( m.edit_config(data, target='running') ) )
+    xmlDom = xml.dom.minidom.parseString( str( m.edit_config(data, target='running') ) )
     # xpath
     #xmlDom = xml.dom.minidom.parseString( str( m.get_config( source='running', filter=('xpath', '/native/hostname')))) 
-    #print (xmlDom.toprettyxml( indent = "  " ))
-    m.edit_config(data, target='running')
+    print (xmlDom.toprettyxml( indent = "  " ))
+    #m.edit_config(data, target='running')
     print("Name Changed!\n")
     
 
